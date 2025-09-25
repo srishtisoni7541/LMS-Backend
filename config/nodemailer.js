81323dd0235d0d78d10d8164521b0d6b0208f547
srishti soni <srishtisoni7541@gmail.com>
2025-09-25 18:34:33 +0530

    done

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -2,6 +2,8 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST || "smtp.gmail.com",
+  port: process.env.EMAIL_PORT || 465,
+  secure: true, // true for 465
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
